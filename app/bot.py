05fd679e678181e2de6ce9eedbe326acddc0fab5
Oskorbin <polozyuk.ser99@gmail.com>
2024-12-31 22:31:02 +0200

    logs: add logger

--- app/bot.py
+++ app/bot.py
@@ -1,45 +1,18 @@
-from telethon import TelegramClient, events, sync
+from telethon import TelegramClient, events
 from config import api_id,api_hash, bot_token
-import logging
-
-
-logging.basicConfig(level=logging.INFO)
-
+from logs.logger import create_logs
 
 # Create the client and connect
 client = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
 
+# create logs
+logger = create_logs()
+
 # Handler for the /start command
 @client.on(events.NewMessage(pattern='/start'))
 async def start(event):
-    await event.respond('Hello! I am a Telethon bot. How can I assist you today?')
-    logging.info(f'Start command received from {event.sender_id}')
-
-
-# Keyword-based response handler
-@client.on(events.NewMessage)
-async def keyword_responder(event):
-    message = event.text.lower()
-    print(message)
-
-    responses = {
-        'hello': 'Hi there! How can I help you today?',
-        '/start': ""
-    }
-
-    response = responses.get(message, None)
-
-    if response or response =="":
-        await event.respond(response)
-    else:
-        # Default response
-        default_response = (
-            "I didn't understand that command. Here are some commands you can try:\n"
-            "/start - Start the bot\n"
-        )
-        await event.respond(default_response)
-        logging.warning(f'Warn response {response}')
-    logging.info(f'Message received from {event.sender_id}: {event.text}')
+    await event.respond('Привет, это бот для поиска вакансий и фриланса!')
+    logger.info(f'Начала команды {event.sender_id}')
 
 # Start the client
 client.start()
